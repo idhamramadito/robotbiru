2fb254bcf6f22fa58b0dc490bb376179f6bac7f0
Martin Hizkia <martinhizkia@gmail.com>
2021-01-26 13:15:03 +0700

    test commit

---
+++ homepage/lib/pages/submenu/pulsa/pulsapage.dart
@@ -0,0 +1,15 @@
+class PulsaPage extends StatefulWidget {
+  PulsaPage({Key key}) : super(key: key);
+
+  @override
+  _PulsaPageState createState() => _PulsaPageState();
+}
+
+class _PulsaPageState extends State<PulsaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
