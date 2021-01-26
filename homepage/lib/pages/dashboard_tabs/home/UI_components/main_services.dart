f2789e79bdd53419263d53c5b12e45a569e8a8dd
Qisas Hasanudin <qisas.tazkia@ui.ac.id>
2021-01-26 14:46:31 +0700

    removed transparent notif bar

--- homepage/lib/pages/dashboard_tabs/home/UI_components/main_services.dart
+++ homepage/lib/pages/dashboard_tabs/home/UI_components/main_services.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:homepage/pages/dashboard_tabs/home/UI_components/other_services.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class MainServices extends StatelessWidget {
   const MainServices({
@@ -20,13 +21,14 @@ class MainServices extends StatelessWidget {
       ),
       height: 280,
       child: GridView.count(
+        shrinkWrap: true,
         physics: NeverScrollableScrollPhysics(),
         crossAxisCount: 3,
         children: [
           InkWell(
             child: Column(
               children: [
-                Image.asset("images/jekbot_n.png", width: 40, height: 40),
+                Image.asset("images/jekbot_n.png", height: 40),
                 Text("JekBot")
               ],
             ),
@@ -35,7 +37,7 @@ class MainServices extends StatelessWidget {
           InkWell(
             child: Column(
               children: [
-                Image.asset("images/taxibot_n.png", width: 40, height: 40),
+                Image.asset("images/taxibot_n.png", height: 40),
                 Text("TaxiBot")
               ],
             ),
@@ -44,7 +46,7 @@ class MainServices extends StatelessWidget {
           InkWell(
             child: Column(
               children: [
-                Image.asset("images/sendbot_n.png", width: 40, height: 40),
+                Image.asset("images/sendbot_n.png", height: 40),
                 Text("SendBot")
               ],
             ),
@@ -53,7 +55,7 @@ class MainServices extends StatelessWidget {
           InkWell(
             child: Column(
               children: [
-                Image.asset("images/foodbot_n.png", width: 40, height: 40),
+                Image.asset("images/foodbot_n.png", height: 40),
                 Text("FoodBot")
               ],
             ),
@@ -62,14 +64,197 @@ class MainServices extends StatelessWidget {
           InkWell(
             child: Column(
               children: [
-                Image.asset("images/pulsa_n.png", width: 40, height: 40),
+                Image.asset("images/pulsa_n.png", height: 40),
                 Text("Pulsa/"),
                 Text("Paket Data")
               ],
             ),
             onTap: () {},
           ),
-          OtherServices(),
+          InkWell(
+            child: Column(
+              children: [
+                Image.asset("images/dll_n.png", width: 40, height: 40),
+                Text("Lainnya")
+              ],
+            ),
+            onTap: () {
+              showMaterialModalBottomSheet(
+                context: context,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                builder: (context) => Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Wrap(
+                    children: [
+                      Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.topCenter,
+                            child: Container(
+                              margin: EdgeInsets.only(top: 10),
+                              height: 4,
+                              width: 30,
+                              decoration: BoxDecoration(
+                                color: Color(0xffC4C4C4),
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 10),
+                            child: Text(
+                              "Semua Menu Robot Biru",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                GestureDetector(
+                                  onTap: () {},
+                                  child: Column(
+                                    children: [
+                                      Image.asset(
+                                        "images/jekbot_n.png",
+                                        width: 47,
+                                        height: 46,
+                                      ),
+                                      Text("JekBot")
+                                    ],
+                                  ),
+                                ),
+                                Column(
+                                  children: [
+                                    Image.asset(
+                                      "images/taxibot_n.png",
+                                      width: 70,
+                                      height: 31,
+                                    ),
+                                    Text("TaxiBot")
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Image.asset(
+                                      "images/sendbot_n.png",
+                                      width: 42,
+                                      height: 47,
+                                    ),
+                                    Text("SendBot")
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                GestureDetector(
+                                  onTap: () {},
+                                  child: Column(
+                                    children: [
+                                      Image.asset(
+                                        "images/foodbot_n.png",
+                                        width: 59,
+                                        height: 34,
+                                      ),
+                                      Text("FoodBot")
+                                    ],
+                                  ),
+                                ),
+                                Column(
+                                  children: [
+                                    Image.asset(
+                                      "images/pulsa_n.png",
+                                      width: 40,
+                                      height: 47,
+                                    ),
+                                    Text("Pulsa/"),
+                                    Text("Paket Data")
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Image.asset(
+                                      "images/martbot_n.png",
+                                      width: 40,
+                                      height: 41,
+                                    ),
+                                    Text("MartBot")
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                GestureDetector(
+                                  onTap: () {},
+                                  child: Column(
+                                    children: [
+                                      Image.asset(
+                                        "images/boxbot_n.png",
+                                        width: 55,
+                                        height: 43,
+                                      ),
+                                      Text("BoxBot")
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  child: Column(
+                                    children: [
+                                      Image.asset(
+                                        "images/tiket_n.png",
+                                        width: 56,
+                                        height: 56,
+                                      ),
+                                      Text("Tiket"),
+                                      Text("Perjalanan")
+                                    ],
+                                  ),
+                                ),
+                                Column(
+                                  children: [
+                                    Image.asset(
+                                      "images/tagihan_n.png",
+                                      width: 56,
+                                      height: 56,
+                                    ),
+                                    Text("Pembayaran"),
+                                    Text("Tagihan")
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 80,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
         ],
       ),
     );
--- homepage/lib/pages/dashboard_tabs/home/home.dart
+++ homepage/lib/pages/dashboard_tabs/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:homepage/pages/dashboard_tabs/home/UI_components/account_balance.dart';
 import 'package:homepage/pages/dashboard_tabs/home/UI_components/complaint_button.dart';
 import 'package:homepage/pages/dashboard_tabs/home/UI_components/infographics.dart';
@@ -8,12 +9,22 @@ import 'package:homepage/pages/dashboard_tabs/home/UI_components/promo_card.dart
 import 'package:homepage/pages/dashboard_tabs/home/UI_components/search_bar.dart';
 import 'package:homepage/pages/dashboard_tabs/home/UI_components/upgrade_to_partner.dart';
 import 'package:homepage/pages/dashboard_tabs/home/UI_components/user_info.dart';
+import 'package:homepage/shared/UI_components/transparent_notif_and_navbar.dart';
 
 class Home extends StatefulWidget {
   _PageDashboard createState() => _PageDashboard();
 }
 
 class _PageDashboard extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.renderView.automaticSystemUiAdjustment = false;
+    SystemChrome.setSystemUIOverlayStyle(
+      transparentNotifBar(),
+    );
+  }
+
   //========================= main function =========================
   @override
   Widget build(BuildContext context) {
