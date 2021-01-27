7390df4bd653cd9288c1938c7bd57d3982df0faa
idhamramadito <idhamadito@gmail.com>
2021-01-27 17:01:55 +0700

    upgrade nominal pulsa

--- homepage/lib/pages/dashboard_tabs/home/submenu/pulsa/components/nominal_pulsa.dart
+++ homepage/lib/pages/dashboard_tabs/home/submenu/pulsa/components/nominal_pulsa.dart
@@ -1,4 +1,4 @@
-    import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class NominalPulsa extends StatefulWidget {
   @override
@@ -6,44 +6,79 @@ class NominalPulsa extends StatefulWidget {
 }
 
 class _NominalPulsaState extends State<NominalPulsa> {
+  List boxAtas = [
+    ['20.000', 'Harga', 'Rp19.500'],
+    ['25.000', 'Harga', 'Rp24.500'],
+    ['30.000', 'Harga', 'Rp29.000']
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-         title: Text(
+        title: Text(
           'Nominal Pulsa',
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
-       ),
-      body: Container(
-      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      child: ButtonTheme(
-        minWidth: 300,
-        height: 50,
-        child: FlatButton(
-            onPressed: () {},
-            color: Color(0xffDDDDDD),
-            shape: RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(10.0)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(left: 10),
-                  child: Text(
-                    "20.000",
-                    style: TextStyle(fontSize: 15, color: Color(0xff818181)),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Container(
-                  child: Icon(Icons.arrow_drop_down),
-                )
-              ],
-            ),
-            ),
       ),
-    )
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        height: 500,
+        child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: (3),
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTap: () {},
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                width: 160,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey, width: 2),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        boxAtas[index][0],
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        boxAtas[index][1],
+                        style: TextStyle(
+                            fontSize: 10,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.grey),
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text(
+                        boxAtas[index][2],
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue[700]),
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
