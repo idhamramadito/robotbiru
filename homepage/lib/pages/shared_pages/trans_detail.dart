b6cad3716f9a57e18840cc0656f43a5415adf1c6
Fadly Ahmad Firdausy <fadlyfirdausy123@gmail.com>
2021-01-28 11:41:16 +0700

    DI PUSH GENG

--- homepage/lib/pages/shared_pages/trans_detail.dart
+++ homepage/lib/pages/shared_pages/trans_detail.dart
@@ -26,28 +26,91 @@ class HasilTrans extends StatelessWidget {
                   child: Image.asset('images/logo_biru.png'),
                 ),
                 Container(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '25 Agustus 2020 11.30 PM',
-                          style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            color: Colors.grey,
-                            fontSize: 12,
-                          ),
+                  padding: const EdgeInsets.all(5.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '25 Agustus 2020 11.30 PM',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey,
+                          fontSize: 12,
                         ),
-                        Text(
-                          'ID: 594T954TY65',
-                          style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            color: Colors.grey,
-                            fontSize: 12,
-                          ),
+                      ),
+                      Text(
+                        'ID: 594T954TY65',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey,
+                          fontSize: 12,
                         ),
-                      ],
-                    ))
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(),
+                Container(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Image.asset('images/ceklis_small.png'),
+                      Text(
+                        '  Transaksi Berhasil',
+                        style: TextStyle(
+                            fontSize: 17, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: const EdgeInsets.all(15.0),
+                  color: Theme.of(context).primaryColor,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Total Pembayaran',
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      Text(
+                        'RP20.300',
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Metode Pembayaran',
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                      ),
+                      Text(
+                        'Saldo Robot Biru',
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.black),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
