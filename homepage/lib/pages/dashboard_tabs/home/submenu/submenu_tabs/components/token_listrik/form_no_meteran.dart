7eed22d8fb76848d83c6c1df3be059078520f31b
Qisas Hasanudin <qisas.tazkia@ui.ac.id>
2021-01-28 16:48:50 +0700

    folder lebih dirapihkan

--- homepage/lib/pages/dashboard_tabs/home/submenu/submenu_tabs/components/token_listrik/form_no_meteran.dart
+++ homepage/lib/pages/dashboard_tabs/home/submenu/submenu_tabs/components/token_listrik/form_no_meteran.dart
@@ -1,68 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class FormFieldNoHandphone extends StatelessWidget {
-  const FormFieldNoHandphone({
+class FormNoMeteran extends StatefulWidget {
+  const FormNoMeteran({
     Key key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.fromLTRB(25, 0, 25, 0),
-            child: Text(
-              "Masukkan Nomor HP",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          ),
-          SizedBox(height: 5),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(25, 0, 25, 0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(
-                  child: TextFormField(
-                    keyboardType: TextInputType.number,
-                    inputFormatters: <TextInputFormatter>[
-                      FilteringTextInputFormatter.allow(
-                        RegExp(r'[0-9]'),
-                      ),
-                    ],
-                    decoration: InputDecoration(
-                      suffixIcon: Icon(Icons.clear, size: 30),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      hintText: "Nomor Handphone",
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: IconButton(
-                    icon: Icon(Icons.perm_contact_cal),
-                    onPressed: () {},
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+  _FormNoMeteranState createState() => _FormNoMeteranState();
 }
 
-class FormFieldNoMeteran extends StatelessWidget {
-  const FormFieldNoMeteran({
-    Key key,
-  }) : super(key: key);
-
+class _FormNoMeteranState extends State<FormNoMeteran> {
   @override
   Widget build(BuildContext context) {
     return Container(
--- homepage/lib/pages/dashboard_tabs/home/submenu/submenu_tabs/input_baru.dart
+++ homepage/lib/pages/dashboard_tabs/home/submenu/submenu_tabs/input_baru.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/card_cashback.dart';
-import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/card_ringkasan.dart';
-import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/formfield_nomor.dart';
-import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/nominal_pulsa.dart';
-import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/nominal_token_listrik.dart';
+import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/pulsa/form_no_handphone.dart';
+import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/pulsa/nominal_pulsa.dart';
+import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/shared/card_cashback.dart';
+import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/shared/card_ringkasan.dart';
+import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/shared/rememberme_checkbox.dart';
+import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/token_listrik/form_no_meteran.dart';
+import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/token_listrik/nominal_token_listrik.dart';
 
 class InputBaru extends StatefulWidget {
   final String pageName;
@@ -14,17 +16,6 @@ class InputBaru extends StatefulWidget {
 }
 
 class _InputBaruState extends State<InputBaru> {
-  bool rememberMe = false;
-
-  void _onRememberMeChanged(bool newValue) => setState(() {
-        rememberMe = newValue;
-        if (rememberMe) {
-          // TODO: Here goes your functionality that remembers the user.
-        } else {
-          // TODO: Forget the user
-        }
-      });
-
   //============================= main function ===============================
   @override
   Widget build(BuildContext context) {
@@ -33,23 +24,8 @@ class _InputBaruState extends State<InputBaru> {
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          (widget.pageName == 'Pulsa')
-              ? FormFieldNoHandphone()
-              : FormFieldNoMeteran(),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(15, 0, 25, 0),
-            child: Row(
-              children: [
-                Checkbox(value: rememberMe, onChanged: _onRememberMeChanged),
-                Text("Simpan Nomor",
-                    style: TextStyle(
-                      fontSize: 13,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.grey,
-                    )),
-              ],
-            ),
-          ),
+          (widget.pageName == 'Pulsa') ? FormNoHandphone() : FormNoMeteran(),
+          RememberMeCheckBox(),
           (widget.pageName == 'Pulsa') ? NominalPulsa() : NominalTokenListrik(),
           CardRingkasan(pageName: widget.pageName),
           Container(height: 10, color: Colors.grey[300]),
