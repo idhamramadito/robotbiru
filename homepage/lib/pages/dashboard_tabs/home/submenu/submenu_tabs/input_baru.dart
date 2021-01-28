999b3b5b38949dc120097f471ee60243b6b37e81
Qisas Hasanudin <qisas.tazkia@ui.ac.id>
2021-01-28 15:29:39 +0700

    nominal pulsa & token dipisah file

--- homepage/lib/pages/dashboard_tabs/home/submenu/submenu_tabs/input_baru.dart
+++ homepage/lib/pages/dashboard_tabs/home/submenu/submenu_tabs/input_baru.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/card_cashback.dart';
 import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/card_ringkasan.dart';
 import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/formfield_nomor.dart';
+import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/nominal_pulsa.dart';
+import 'package:homepage/pages/dashboard_tabs/home/submenu/submenu_tabs/components/nominal_token_listrik.dart';
 
 class InputBaru extends StatefulWidget {
   final String pageName;
@@ -13,25 +15,6 @@ class InputBaru extends StatefulWidget {
 
 class _InputBaruState extends State<InputBaru> {
   bool rememberMe = false;
-  int selectedIndex = 0;
-  String selectedValue = '20.000';
-  List boxPulsa = [
-    ['20.000', 'Harga', 'Rp19.500'],
-    ['25.000', 'Harga', 'Rp24.500'],
-    ['30.000', 'Harga', 'Rp29.000']
-  ];
-  List _boxTokenListrik = [
-    ['20.000', 'Cashback: Rp 750.00', 'Rp21.750'],
-    ['50.000', 'Cashback: Rp 750.00', 'Rp51.750'],
-    ['75.000', 'Cashback: Rp 750.00', 'Rp75.750'],
-    ['100.000', 'Cashback: Rp 750.00', 'Rp101.750'],
-    ['200.000', 'Cashback: Rp 750.00', 'Rp201.750'],
-    ['500.000', 'Cashback: Rp 750.00', 'Rp501.750'],
-    ['1.000.000', 'Cashback: Rp 750.00', 'Rp1.001.750'],
-    ['5.000.000', 'Cashback: Rp 750.00', 'Rp5.001.750'],
-    ['10.000.000', 'Cashback: Rp 750.00', 'Rp10.001.750'],
-    ['50.000.000', 'Cashback: Rp 750.00', 'Rp50.001.750'],
-  ];
 
   void _onRememberMeChanged(bool newValue) => setState(() {
         rememberMe = newValue;
@@ -67,7 +50,7 @@ class _InputBaruState extends State<InputBaru> {
               ],
             ),
           ),
-          (widget.pageName == 'Pulsa') ? nominalPulsa() : nominalTokenListrik(),
+          (widget.pageName == 'Pulsa') ? NominalPulsa() : NominalTokenListrik(),
           CardRingkasan(pageName: widget.pageName),
           Container(height: 10, color: Colors.grey[300]),
           CardCashback(),
@@ -76,130 +59,4 @@ class _InputBaruState extends State<InputBaru> {
     );
   }
   //============================= main function ===============================
-
-  Container nominalPulsa() {
-    return Container(
-      child: Column(
-        children: [
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.fromLTRB(25, 0, 25, 0),
-            child: Text(
-              "Nominal Pulsa",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: DropdownButtonFormField(
-              iconEnabledColor: Theme.of(context).primaryColor,
-              decoration: InputDecoration(
-                isDense: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              value: selectedValue,
-              items: boxPulsa.map((element) {
-                return DropdownMenuItem(
-                  child: Text(element[0]),
-                  value: element[0],
-                );
-              }).toList(),
-              onChanged: (value) {
-                setState(() {
-                  selectedValue = value;
-                });
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Container nominalTokenListrik() {
-    return Container(
-      child: Column(
-        children: [
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.fromLTRB(25, 0, 25, 0),
-            child: Text(
-              "Nominal Token Listrik",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Container(
-            height: 200,
-            margin: EdgeInsets.symmetric(vertical: 10),
-            child: GridView.builder(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemCount: _boxTokenListrik.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, childAspectRatio: 0.55),
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-                    setState(() {
-                      selectedIndex = index;
-                      selectedValue = _boxTokenListrik[selectedIndex][0];
-                    });
-                  },
-                  child: Container(
-                    width: 500,
-                    margin: EdgeInsets.all(5),
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(
-                          width: 2,
-                          color: (selectedIndex == index)
-                              ? Colors.blue
-                              : Colors.grey[200],
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            _boxTokenListrik[index][0],
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            _boxTokenListrik[index][1],
-                            style: TextStyle(
-                                fontSize: 10,
-                                fontWeight: FontWeight.normal,
-                                color: Colors.grey),
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: Text(
-                            _boxTokenListrik[index][2],
-                            style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.blue[700]),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
 }
