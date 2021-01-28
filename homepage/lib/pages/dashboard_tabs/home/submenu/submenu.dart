9a6526ec9683aaedf57db9048e58e46d717b0af8
Qisas Hasanudin <qisas.tazkia@ui.ac.id>
2021-01-28 16:00:05 +0700

    added button to change payment method (WIP)

--- homepage/lib/pages/dashboard_tabs/home/submenu/submenu.dart
+++ homepage/lib/pages/dashboard_tabs/home/submenu/submenu.dart
@@ -17,6 +17,7 @@ class SubMenu extends StatefulWidget {
 class _SubMenuState extends State<SubMenu> {
   String currency = 'Rp';
   double chosenPrice = 20000.00;
+  double accountBalance = 12000.0;
 
   @override
   Widget build(BuildContext context) {
@@ -55,48 +56,103 @@ class _SubMenuState extends State<SubMenu> {
             ),
           ],
         ),
-        bottomNavigationBar: Container(
-          height: 90,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(blurRadius: 10, color: Colors.black.withOpacity(0.1))
-            ],
-          ),
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Column(
-              children: [
-                Container(
-                  alignment: Alignment.centerLeft,
-                  child: RichText(
-                    text: TextSpan(
-                      text: 'Total Akhir:  ',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 17,
+        bottomNavigationBar: CheckoutBottomBar(
+          currency: currency,
+          accountBalance: accountBalance,
+          chosenPrice: chosenPrice,
+        ),
+      ),
+    );
+  }
+}
+
+class CheckoutBottomBar extends StatefulWidget {
+  const CheckoutBottomBar({
+    Key key,
+    @required this.currency,
+    @required this.accountBalance,
+    @required this.chosenPrice,
+  }) : super(key: key);
+
+  final String currency;
+  final double accountBalance;
+  final double chosenPrice;
+
+  @override
+  _CheckoutBottomBarState createState() => _CheckoutBottomBarState();
+}
+
+class _CheckoutBottomBarState extends State<CheckoutBottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(blurRadius: 10, color: Colors.black.withOpacity(0.1))
+        ],
+      ),
+      child: Column(
+        children: [
+          InkWell(
+            onTap: () {
+              Navigator.of(context).pushNamed('/payment_method');
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 5.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Image.asset("images/dompet.png", width: 30),
+                      SizedBox(width: 10),
+                      Text(
+                        "Saldo",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 17),
                       ),
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: '$currency ${chosenPrice.toString()}',
-                          style: TextStyle(color: Colors.blue),
-                        ),
-                      ],
-                    ),
+                    ],
                   ),
-                ),
-                BigButton(
-                  title: 'Beli',
-                  route: () {
-                    Navigator.of(context)
-                        .pushNamed('/pin_code', arguments: 'Masukkan Pin Anda');
-                  },
-                ),
-              ],
+                  Text(
+                    '${widget.currency} ${widget.accountBalance.toString()}',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
+          Divider(thickness: 2),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 5),
+            alignment: Alignment.centerLeft,
+            child: RichText(
+              text: TextSpan(
+                text: 'Total Akhir:  ',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '${widget.currency} ${widget.chosenPrice.toString()}',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BigButton(
+            title: 'Beli',
+            route: () {
+              Navigator.of(context)
+                  .pushNamed('/pin_code', arguments: 'Masukkan Pin Anda');
+            },
+          ),
+        ],
       ),
     );
   }
