c0e63ce7d58ce0ec7bdf0320ed10ca9b3ac8cb80
Qisas Hasanudin <qisas.tazkia@ui.ac.id>
2021-01-28 21:13:53 +0700

    minor fix

--- homepage/lib/pages/dashboard_tabs/home/transaction_process/trans_details.dart
+++ homepage/lib/pages/dashboard_tabs/home/transaction_process/trans_details.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:homepage/pages/dashboard_tabs/home/transaction_process/invoice/invoice_pulsa.dart';
-
-import 'invoice/invoice_tokenlistrik.dart';
+import 'package:homepage/pages/dashboard_tabs/home/transaction_process/invoice/invoice_tokenlistrik.dart';
 
 class TransDetails extends StatelessWidget {
   final String pageName;
