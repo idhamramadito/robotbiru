a9c0c362d96776e49345de7fd69bb9e7a7d6d0f6
Qisas Hasanudin <qisas.tazkia@ui.ac.id>
2021-01-29 14:54:33 +0700

    beres

--- homepage/lib/shared_UI_components/card_cashback.dart
+++ homepage/lib/shared_UI_components/card_cashback.dart
@@ -24,29 +24,22 @@ class _CardCashbackState extends State<CardCashback> {
               style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-            child: new Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [new Text("Pemilik Retail"), new Text("Rp5800")]),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-            child: new Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [new Text("Badan Koperasi"), new Text("Rp5800")]),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-            child: new Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [new Text("Anggota Koperasi"), new Text("Rp5800")]),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-            child: new Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [new Text("Anggota Retail"), new Text("Rp5800")]),
+          Container(
+            height: 150,
+            child: ListView.builder(
+                itemCount: _cashback.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 25, vertical: 10),
+                    child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          new Text(_cashback[index][0]),
+                          new Text(_cashback[index][1]),
+                        ]),
+                  );
+                }),
           ),
         ],
       ),
