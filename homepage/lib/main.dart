f881b9e365ac58ce0af4de92af3519671237784c
Qisas Hasanudin <qisas.tazkia@ui.ac.id>
2021-01-30 07:28:08 +0700

    minor fix

--- homepage/lib/main.dart
+++ homepage/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:homepage/routing/routing_table.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   runApp(RobotBiruApp());
 }
 
