ed633f4e033364b26ff0d6dc7bfe99a1b8a7bf4d
idhamramadito <idhamadito@gmail.com>
2021-02-01 14:55:59 +0700

    kalender di date_page

--- homepage/lib/pages/dashboard_tabs/home/submenu/pesawat/date_page.dart
+++ homepage/lib/pages/dashboard_tabs/home/submenu/pesawat/date_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 class DatePage extends StatefulWidget {
   DatePage({
@@ -13,7 +14,51 @@ class DatePage extends StatefulWidget {
 }
 
 class _DatePageState extends State<DatePage> {
+  String _selectedDate;
+  String _dateCount;
+  String _range;
+  String _rangeCount;
+
   @override
+  void initState() {
+    _selectedDate = '';
+    _dateCount = '';
+    _range = '';
+    _rangeCount = '';
+    super.initState();
+  }
+
+  void _onSelectionChanged(DateRangePickerSelectionChangedArgs args) {
+    /// The argument value will return the changed date as [DateTime] when the
+    /// widget [SfDateRangeSelectionMode] set as single.
+    ///
+    /// The argument value will return the changed dates as [List<DateTime>]
+    /// when the widget [SfDateRangeSelectionMode] set as multiple.
+    ///
+    /// The argument value will return the changed range as [PickerDateRange]
+    /// when the widget [SfDateRangeSelectionMode] set as range.
+    ///
+    /// The argument value will return the changed ranges as
+    /// [List<PickerDateRange] when the widget [SfDateRangeSelectionMode] set as
+    /// multi range.
+    setState(() {
+      if (args.value is PickerDateRange) {
+        _range =
+            DateFormat('dd/MM/yyyy').format(args.value.startDate).toString() +
+                ' - ' +
+                DateFormat('dd/MM/yyyy')
+                    .format(args.value.endDate ?? args.value.startDate)
+                    .toString();
+      } else if (args.value is DateTime) {
+        _selectedDate = args.value;
+      } else if (args.value is List<DateTime>) {
+        _dateCount = args.value.length.toString();
+      } else {
+        _rangeCount = args.value.length.toString();
+      }
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -23,23 +68,37 @@ class _DatePageState extends State<DatePage> {
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-            color: Theme.of(context).primaryColor,
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
-              child: TextField(
-                decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.search, size: 25),
-                    hintText: 'Cari Kota atau Bandara'),
-              ),
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            height: 70,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
             ),
           ),
+          Positioned(
+            left: 0,
+            top: 80,
+            right: 0,
+            bottom: 0,
+            child: SfDateRangePicker(
+              onSelectionChanged: _onSelectionChanged,
+              selectionMode: DateRangePickerSelectionMode.multiRange,
+              initialSelectedRange: PickerDateRange(
+                  DateTime.now().subtract(const Duration()),
+                  DateTime.now().add(const Duration())),
+            ),
+          )
         ],
       ),
     );
   }
+
+  // ignore: non_constant_identifier_names
+  DateFormat(String s) async {}
 }
