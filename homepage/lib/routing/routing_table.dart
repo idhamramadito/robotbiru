ee4a2ed192ce2d2e6121b71eaf68b9c9f521d5cb
Fadly Ahmad Firdausy <fadlyfirdausy123@gmail.com>
2021-02-02 12:09:29 +0700

    kelas kabi

--- homepage/lib/routing/routing_table.dart
+++ homepage/lib/routing/routing_table.dart
@@ -3,7 +3,6 @@ import 'package:homepage/pages/dashboard_tabs/home/submenu/pesawat/jadwal_pesawa
 import 'package:homepage/pages/dashboard_tabs/home/submenu/pesawat/page_pesawat.dart';
 import 'package:homepage/pages/dashboard_tabs/home/submenu/pesawat/search_page.dart';
 import 'package:homepage/pages/dashboard_tabs/home/submenu/pesawat/date_page.dart';
-import 'package:homepage/pages/dashboard_tabs/home/submenu/pesawat/kelas_kabin.dart';
 import 'package:homepage/routing/error_route.dart';
 import 'package:homepage/pages/dashboard.dart';
 import 'package:homepage/pages/dashboard_tabs/home/submenu/pulsa/page_pulsa.dart';
@@ -25,8 +24,6 @@ class RoutingTable {
         return MaterialPageRoute(builder: (_) => PagePulsa());
       case '/token_listrik':
         return MaterialPageRoute(builder: (_) => PageTokenListrik());
-      case '/cabin_class':
-        return MaterialPageRoute(builder: (_) => KelasKabin());
       case '/pesawat':
         return MaterialPageRoute(builder: (_) => PagePesawat());
       case '/search_page':
