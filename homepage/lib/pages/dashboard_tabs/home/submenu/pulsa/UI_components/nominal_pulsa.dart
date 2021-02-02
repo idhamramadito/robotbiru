2a0919d8c2ed73b09abced5e50bc62335f82f96e
Qisas Hasanudin <qisas.tazkia@ui.ac.id>
2021-02-02 09:54:50 +0700

    UI improvements

--- homepage/lib/pages/dashboard_tabs/home/submenu/pulsa/UI_components/nominal_pulsa.dart
+++ homepage/lib/pages/dashboard_tabs/home/submenu/pulsa/UI_components/nominal_pulsa.dart
@@ -31,18 +31,51 @@ class _NominalPulsaState extends State<NominalPulsa> {
           ),
           Container(
             margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: DropdownButtonFormField(
+            child: DropdownButton(
+              isExpanded: true,
               iconEnabledColor: Theme.of(context).primaryColor,
-              decoration: InputDecoration(
-                isDense: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
               value: selectedValue,
               items: _boxPulsa.map((element) {
                 return DropdownMenuItem(
-                  child: Text(element.toString()),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    margin: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.grey, width: 2),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '$element',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.left,
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              'Harga',
+                              style: TextStyle(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.grey),
+                              textAlign: TextAlign.right,
+                            ),
+                            Text(
+                              '${element - 500}',
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue[700]),
+                              textAlign: TextAlign.right,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                   value: element,
                 );
               }).toList(),
