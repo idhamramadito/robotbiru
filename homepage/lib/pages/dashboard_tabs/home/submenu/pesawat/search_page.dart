9d37330d9298a8ab377518f757d204dd4d89c342
Fadly Ahmad Firdausy <fadlyfirdausy123@gmail.com>
2021-02-02 15:25:15 +0700

    push halaman babi

--- homepage/lib/pages/dashboard_tabs/home/submenu/pesawat/search_page.dart
+++ homepage/lib/pages/dashboard_tabs/home/submenu/pesawat/search_page.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+List _tujuan = [
+  ['images/gedung.png', 'Jakarta, Indonesia', 'Semua Bandara', 'JKTC'],
+  ['images/gedung.png', 'Surabaya, Indonesia', 'Semua Bandara', 'SBYC'],
+  ['images/gedung.png', 'Medan, Indonesia', 'Semua Bandara', 'MESC'],
+];
+
 class SearchPage extends StatefulWidget {
   SearchPage({
     Key key,
@@ -20,122 +26,80 @@ class _SearchPageState extends State<SearchPage> {
         elevation: 0,
         title: Text('Pilih ${widget.pageName}'),
       ),
-      body: Column(
-        children: [
-          Container(
-            color: Theme.of(context).primaryColor,
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
-              child: TextField(
-                decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.search, size: 25),
-                    hintText: (widget.pageName == 'Maskapai')
-                        ? 'Cari Maskapai'
-                        : 'Cari Kota atau Bandara'),
-              ),
-            ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.fromLTRB(30, 10, 20, 0),
-            child: Text((widget.pageName == 'Maskapai')
-                ? 'Semua Maskapai'
-                : 'Tujuan Terpopuler'),
-          ),
-          SizedBox(height: 10),
-          Container(
-            child: ListTile(
-              dense: true,
-              leading: Image.asset("images/gedung.png", width: 40),
-              title: Text(
-                'Jakarta, Indonesia',
-                style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(
-                'Semua Bandara',
-                style: TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.normal,
-                    color: Colors.grey),
-              ),
-              trailing: Container(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: Theme.of(context).primaryColor,
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                 decoration: BoxDecoration(
-                    color: Colors.grey[200],
+                    color: Colors.white,
                     borderRadius: BorderRadius.circular(10)),
-                child: Text(
-                  'JKTC',
-                  style: TextStyle(
-                    color: Colors.grey[700],
-                  ),
+                child: TextField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search, size: 25),
+                      hintText: (widget.pageName == 'Maskapai')
+                          ? 'Cari Maskapai'
+                          : 'Cari Kota atau Bandara'),
                 ),
               ),
             ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            child: ListTile(
-              dense: true,
-              leading: Image.asset("images/gedung.png", width: 40),
-              title: Text(
-                'Surabaya, Indonesia',
-                style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(
-                'Semua Bandara',
-                style: TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.normal,
-                    color: Colors.grey),
-              ),
-              trailing: Container(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(10)),
-                child: Text(
-                  'SBYC',
-                  style: TextStyle(
-                    color: Colors.grey[700],
-                  ),
-                ),
-              ),
+            SizedBox(height: 10),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.fromLTRB(30, 10, 20, 0),
+              child: Text((widget.pageName == 'Maskapai')
+                  ? 'Semua Maskapai'
+                  : 'Tujuan Terpopuler'),
             ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            child: ListTile(
-              dense: true,
-              leading: Image.asset("images/gedung.png", width: 40),
-              title: Text(
-                'Medan, Indonesia',
-                style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(
-                'Semua Bandara',
-                style: TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.normal,
-                    color: Colors.grey),
-              ),
-              trailing: Container(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(10)),
-                child: Text(
-                  'MESC',
-                  style: TextStyle(
-                    color: Colors.grey[700],
-                  ),
-                ),
-              ),
+            SizedBox(height: 10),
+            ListView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: 3,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ListTile(
+                      onTap: () {
+                        Navigator.pop(context, _tujuan[index][1]);
+                      },
+                      dense: true,
+                      leading: Image.asset(_tujuan[index][0], width: 40),
+                      title: Text(
+                        _tujuan[index][1],
+                        style: TextStyle(
+                            fontSize: 17, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        _tujuan[index][2],
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.grey),
+                      ),
+                      trailing: Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Text(
+                          _tujuan[index][3],
+                          style: TextStyle(
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
