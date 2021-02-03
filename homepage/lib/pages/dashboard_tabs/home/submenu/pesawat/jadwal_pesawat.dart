3bd549c2f18dc02a94779d5d344aaf099b85f858
Martin Hizkia <martinhizkia@gmail.com>
2021-02-03 16:36:18 +0700

    modalsheetbottom (msh ada bug)!

--- homepage/lib/pages/dashboard_tabs/home/submenu/pesawat/jadwal_pesawat.dart
+++ homepage/lib/pages/dashboard_tabs/home/submenu/pesawat/jadwal_pesawat.dart
@@ -1,5 +1,161 @@
-import 'package:flutter/material.dart';
 import 'package:homepage/pages/dashboard_tabs/home/submenu/pesawat/UI_components/ticket_details.dart';
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class ModalBottomSheet extends StatefulWidget {
+  _ModalBottomSheetState createState() => _ModalBottomSheetState();
+}
+
+class _ModalBottomSheetState extends State<ModalBottomSheet>
+    with SingleTickerProviderStateMixin {
+  List _rekomendasi = [
+    "Harga Terendah",
+    "Keberangkatan Paling Awal",
+    "Keberangkatan Paling Akhir",
+    "Kedatangan Paling Awal",
+    "Kedatangan Paling Akhir",
+    "Durasi Tercepat"
+  ];
+  bool rememberMe = false;
+  List _filterDengan = [
+    ["Langsung", false],
+    ["1 Transit", false],
+    ["2 Transit", false],
+  ];
+
+  void _onRememberMeChanged(bool newValue) => setState(() {
+        rememberMe = newValue;
+        if (rememberMe) {
+          // TODO: Here goes your functionality that remembers the user.
+        } else {
+          // TODO: Forget the user
+        }
+      });
+
+  Widget build(BuildContext context) {
+    return Container(
+        child: DraggableScrollableSheet(
+      expand: false,
+      builder: (context, controller) {
+        return SingleChildScrollView(
+          controller: controller,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(25, 35, 25, 0),
+            child: Column(
+              //crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 15),
+                  child: Column(children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                      child: Container(
+                          child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text("Urutkan & Filter"),
+                        ],
+                      )),
+                    )
+                  ]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text("Urutkan"),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          width: 342,
+                          child: ExpansionTile(
+                            title: Text(
+                              "Rekomendasi",
+                            ),
+                            children: <Widget>[
+                              ListView.builder(
+                                  shrinkWrap: true,
+                                  itemCount: _rekomendasi.length,
+                                  itemBuilder: (context, index) {
+                                    return Padding(
+                                      padding: const EdgeInsets.fromLTRB(
+                                          0, 0, 0, 10),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.fromLTRB(
+                                                0, 0, 0, 10),
+                                            child: Text(_rekomendasi[index]),
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  })
+                            ],
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                Divider(),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: Text("Filter Dengan"),
+                ),
+                Column(
+                  children: [
+                    Column(
+                      children: [
+                        Text("Durasi Transit"),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                ListView.builder(
+                                    shrinkWrap: true,
+                                    itemCount: _filterDengan.length,
+                                    itemBuilder: (context, index) {
+                                      return Container(
+                                          child: Row(
+                                        children: [
+                                          new Text(
+                                            _filterDengan[index][0],
+                                            style:
+                                                TextStyle(color: Colors.black),
+                                          ),
+                                        ],
+                                      ));
+                                    })
+                              ]),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    ));
+  }
+}
 
 class JadwalPesawat extends StatefulWidget {
   final List previousData;
@@ -15,6 +171,15 @@ class JadwalPesawat extends StatefulWidget {
 
 class _JadwalPesawatState extends State<JadwalPesawat> {
   List<bool> isSelected = [false, false];
+
+  List _rekomendasi = [
+    "Harga Terendah",
+    "Keberangkatan Paling Awal",
+    "Keberangkatan Paling Akhir",
+    "Kedatangan Paling Awal",
+    "Kedatangan Paling Akhir",
+    "Durasi Tercepat"
+  ];
 
   List _filterButton = ["Langsung", "Gratis Bagasi", "Makanan Gratis"];
   List _cardJadwal = [
@@ -375,7 +540,19 @@ class _JadwalPesawatState extends State<JadwalPesawat> {
           constraints: BoxConstraints(minHeight: 36.0),
           isSelected: isSelected,
           onPressed: (index) {
-            // Respond to button selection
+            switch (index) {
+              case 0:
+                showModalBottomSheet(
+                  context: context,
+                  builder: (_) => ModalBottomSheet(),
+                );
+                break;
+              case 1:
+                print("1");
+                break;
+              default:
+                print("Error");
+            }
             setState(() {
               isSelected[index] = !isSelected[index];
               if (index == 1) {
