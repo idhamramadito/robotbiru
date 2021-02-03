67b78434bfbb299a23dfb791ff7a1aa12086995d
Fadly Ahmad Firdausy <fadlyfirdausy123@gmail.com>
2021-02-03 13:39:15 +0700

    tiket

--- homepage/lib/pages/dashboard_tabs/home/submenu/pesawat/UI_components/ticket_details.dart
+++ homepage/lib/pages/dashboard_tabs/home/submenu/pesawat/UI_components/ticket_details.dart
@@ -78,7 +78,170 @@ Future ticketDetails(BuildContext context) {
                         ],
                       ),
                     ),
-                    Placeholder(),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ListTile(
+                            dense: true,
+                            leading: Image.asset('images/japan_airlines.png'),
+                            title: Text(
+                              'Japan Airlines',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
+                              ),
+                            ),
+                            subtitle: Text(
+                              'JT-22',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                  fontSize: 15,
+                                  color: Colors.grey),
+                            ),
+                            trailing: Container(
+                              width: 100,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Flexible(
+                                      child:
+                                          Image.asset('images/gray_clock.png')),
+                                  Flexible(
+                                    child: Text(
+                                      '1j 50m',
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Divider(),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                Text(
+                                  'Bagasi Kabin',
+                                  style: TextStyle(fontSize: 17),
+                                ),
+                                SizedBox(width: 25),
+                                Text(
+                                  '7Kg',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 17),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Divider(),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Fasilitas',
+                                  style: TextStyle(fontSize: 17),
+                                ),
+                                Column(
+                                  children: [
+                                    Container(
+                                      width: 250,
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Flexible(
+                                              child: Image.asset(
+                                                  'images/koper_biru.png')),
+                                          SizedBox(width: 25),
+                                          Flexible(
+                                            child: Text(
+                                              'Bagasi',
+                                              style: TextStyle(fontSize: 15),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 250,
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Flexible(
+                                              child: Image.asset(
+                                                  'images/garpu_biru.png')),
+                                          SizedBox(width: 25),
+                                          Flexible(
+                                            child: Text(
+                                              'Makanan',
+                                              style: TextStyle(fontSize: 15),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 250,
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Flexible(
+                                              child: Image.asset(
+                                                  'images/media_biru.png')),
+                                          SizedBox(width: 25),
+                                          Flexible(
+                                            child: Text(
+                                              'Hiburan',
+                                              style: TextStyle(fontSize: 15),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 250,
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Flexible(
+                                              child: Image.asset(
+                                                  'images/refundable.png')),
+                                          SizedBox(width: 25),
+                                          Flexible(
+                                            child: Text(
+                                              'Refundable',
+                                              style: TextStyle(fontSize: 15),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
                     Placeholder(),
                   ],
                 ),
