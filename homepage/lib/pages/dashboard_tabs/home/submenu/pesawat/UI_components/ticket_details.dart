5766dac1537b8c05f1b3fdec32f9fee12bf8f2f2
Qisas Hasanudin <qisas.tazkia@ui.ac.id>
2021-02-03 11:25:31 +0700

    minor fix

--- homepage/lib/pages/dashboard_tabs/home/submenu/pesawat/UI_components/ticket_details.dart
+++ homepage/lib/pages/dashboard_tabs/home/submenu/pesawat/UI_components/ticket_details.dart
@@ -5,6 +5,7 @@ import 'package:homepage/shared/shared_UI_components/slide_up_marker.dart';
 
 Future ticketDetails(BuildContext context) {
   return showModalBottomSheet(
+    isScrollControlled: true,
     context: context,
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.only(
@@ -15,38 +16,87 @@ Future ticketDetails(BuildContext context) {
     builder: (context) {
       return DefaultTabController(
         length: 3,
-        child: Column(
-          children: [
-            SlideUpMarker(),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-              child: Text(
-                'Detail Tiket',
-                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.9,
+          child: Column(
+            children: [
+              SlideUpMarker(),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                child: Text(
+                  'Detail Tiket',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: TabBar(
-                labelColor: Colors.black,
-                unselectedLabelColor: Colors.grey,
-                tabs: [
-                  Tab(text: "Perjalanan"),
-                  Tab(text: "Fasilitas"),
-                  Tab(text: "Harga"),
-                ],
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: TabBar(
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.grey,
+                  tabs: [
+                    Tab(text: "Perjalanan"),
+                    Tab(text: "Fasilitas"),
+                    Tab(text: "Harga"),
+                  ],
+                ),
               ),
-            ),
-            Flexible(
-              child: TabBarView(
-                children: [
-                  Placeholder(),
-                  Placeholder(),
-                  Placeholder(),
-                ],
+              Flexible(
+                child: TabBarView(
+                  children: [
+                    Placeholder(),
+                    Placeholder(),
+                    Placeholder(),
+                  ],
+                ),
               ),
-            ),
-          ],
+              Container(
+                height: 70,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 10, color: Colors.black.withOpacity(0.1))
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    Flexible(
+                      child: Container(
+                        padding: EdgeInsets.symmetric(vertical: 5),
+                        alignment: Alignment.centerLeft,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Rp 307.000',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue,
+                              ),
+                            ),
+                            Text(
+                              "per orang",
+                              style: TextStyle(
+                                color: Colors.grey[500],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      child: BigButton(
+                        title: 'Beli',
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       );
     },
