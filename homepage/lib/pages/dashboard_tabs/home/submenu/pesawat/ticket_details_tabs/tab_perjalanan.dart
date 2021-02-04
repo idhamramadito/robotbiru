4ddf871f29d88b3cd5c8bcd2162347dfad958bee
Qisas Hasanudin <qisas.tazkia@ui.ac.id>
2021-02-04 11:38:20 +0700

    tab detail tiket

--- homepage/lib/pages/dashboard_tabs/home/submenu/pesawat/ticket_details_tabs/tab_perjalanan.dart
+++ homepage/lib/pages/dashboard_tabs/home/submenu/pesawat/ticket_details_tabs/tab_perjalanan.dart
@@ -46,7 +46,7 @@ class TabPerjalanan extends StatelessWidget {
                 ),
               ),
             ),
-            Divider(),
+            Divider(thickness: 2),
             Row(
               children: [
                 Flexible(
@@ -123,8 +123,148 @@ class TabPerjalanan extends StatelessWidget {
                 ),
               ],
             ),
-            Divider(),
-            SizedBox(height: 10),
+            SizedBox(height: 20),
+            Container(
+              alignment: Alignment.center,
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: RichText(
+                text: TextSpan(
+                  text: '0j 50m  ',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'transit di Surabaya (SBY)',
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ListTile(
+              dense: true,
+              leading: Image.asset('images/japan_airlines.png'),
+              title: Text(
+                'Japan Airlines',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17,
+                ),
+              ),
+              subtitle: Text(
+                'JT-22',
+                style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 15,
+                    color: Colors.grey),
+              ),
+              trailing: Container(
+                width: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Flexible(child: Image.asset('images/gray_clock.png')),
+                    Flexible(
+                      child: Text(
+                        '1j 50m',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Divider(thickness: 2),
+            Row(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Image.asset(
+                    'images/origin_to_destination.png',
+                    height: 200,
+                  ),
+                ),
+                Flexible(
+                  flex: 6,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Flexible(
+                            child: ListTile(
+                              title: Text(
+                                '05:00',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text('Senin, 12 Jan 2021'),
+                            ),
+                          ),
+                          Flexible(
+                            child: ListTile(
+                              title: Text(
+                                'Jakarta (CGK)',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text('Soekarno Hatta'),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Flexible(
+                            child: ListTile(
+                              title: Text(
+                                '1j 50m',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text('Langsung'),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Flexible(
+                            child: ListTile(
+                              title: Text(
+                                '07:00',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text('Senin, 12 Jan 2021'),
+                            ),
+                          ),
+                          Flexible(
+                            child: ListTile(
+                              title: Text(
+                                'Denpasar (DPS)',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text('Ngurah Rai'),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Divider(thickness: 2),
             InkWell(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -143,6 +283,7 @@ class TabPerjalanan extends StatelessWidget {
                 Navigator.of(context).pushNamed('/kebijakan_pembatalan');
               },
             ),
+            Divider(thickness: 2),
           ],
         ),
       ),
