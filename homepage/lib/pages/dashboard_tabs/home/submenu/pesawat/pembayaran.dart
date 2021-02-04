337051605bbd0541d6e6452314964e4f0b1f67c5
Fadly Ahmad Firdausy <fadlyfirdausy123@gmail.com>
2021-02-04 11:04:36 +0700

    push pembayaran

--- homepage/lib/pages/dashboard_tabs/home/submenu/pesawat/pembayaran.dart
+++ homepage/lib/pages/dashboard_tabs/home/submenu/pesawat/pembayaran.dart
@@ -38,90 +38,187 @@ class _PembayaranState extends State<Pembayaran> {
           ],
         ),
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 15),
-        margin: EdgeInsets.only(top: 15),
-        child: Column(
-          children: [
-            InkWell(
-              onTap: () {
-                ticketDetails(
-                    context, widget.data[0], widget.data[1], 'Pemesanan');
-              },
-              child: Container(
-                margin: EdgeInsets.only(top: 20),
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          margin: EdgeInsets.only(top: 15),
+          child: Column(
+            children: [
+              InkWell(
+                onTap: () {
+                  ticketDetails(
+                      context, widget.data[0], widget.data[1], 'Pemesanan');
+                },
+                child: Container(
+                  margin: EdgeInsets.only(top: 20),
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(15.0))),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Image.asset('images/pesawat_kecil.png'),
+                          Text(
+                            'Jakarta',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Icon(
+                            Icons.arrow_right_alt,
+                            size: 30,
+                          ),
+                          Text(
+                            'Denpasar - Bali',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Icon(Icons.keyboard_arrow_down, size: 30)
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
                 decoration: BoxDecoration(
                     border: Border.all(color: Colors.grey),
                     borderRadius:
-                        BorderRadius.vertical(top: Radius.circular(15.0))),
+                        BorderRadius.vertical(bottom: Radius.circular(15.0))),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(
-                      children: [
-                        Image.asset('images/pesawat_kecil.png'),
-                        Text(
-                          'Jakarta',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                        Icon(
-                          Icons.arrow_right_alt,
-                          size: 30,
-                        ),
-                        Text(
-                          'Denpasar - Bali',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ],
+                    Text(
+                      'Total Pembayaran',
+                      style: TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.normal),
                     ),
-                    Icon(Icons.keyboard_arrow_down, size: 30)
+                    Text(
+                      'Rp 445.565',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ],
                 ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius:
-                      BorderRadius.vertical(bottom: Radius.circular(15.0))),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Total Pembayaran',
-                    style:
-                        TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
-                  ),
-                  Text(
-                    'Rp 445.565',
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Pilih Metode Pembayaran',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Pilih Metode Pembayaran',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              SizedBox(height: 20),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemExtent: 100,
+                itemCount: 3,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Radio(
+                      value: _atm[index],
+                      groupValue: selectedValue,
+                      onChanged: (value) {
+                        setState(() {
+                          selectedValue = value;
+                        });
+                      },
+                    ),
+                    title: Image.asset(_atm[index]),
+                    trailing: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Transfer Bank Mandiri',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        Text('1320015081202',
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold)),
+                        Text(
+                          'PT EDUMATIC INTERNASIONAL',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        Text(
+                          'Jam Operasional 05:00:00-22:30:00 WIB',
+                          style: TextStyle(fontSize: 10),
+                        ),
+                      ],
+                    ),
+                  );
+                },
               ),
-            ),
-            SizedBox(height: 20),
-          ],
+              ListTile(
+                leading: Radio(
+                  value: 'Saldo',
+                  groupValue: selectedValue,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedValue = value;
+                    });
+                  },
+                ),
+                title: Text(
+                  'Saldo',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                    'Saldo Anda akan otomatis terpotong untuk pembayaran Tiket Pesawat'),
+                trailing: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Rp 50.000',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ),
+              // ListTile(
+              //   leading: Radio(
+              //     value: _atm[1],
+              //     groupValue: selectedValue,
+              //     onChanged: (value) {
+              //       setState(() {
+              //         selectedValue = value;
+              //       });
+              //     },
+              //   ),
+              //   title: Image.asset(_atm[1]),
+              // ),
+              // SizedBox(height: 20),
+              // ListTile(
+              //   leading: Radio(
+              //     value: _atm[2],
+              //     groupValue: selectedValue,
+              //     onChanged: (value) {
+              //       setState(() {
+              //         selectedValue = value;
+              //       });
+              //     },
+              //   ),
+              //   title: Image.asset(_atm[2]),
+              // ),
+              // SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+List _atm = ['images/mandiri.png', 'images/bca.png', 'images/bjb.png', 'Saldo'];
+var selectedValue;
