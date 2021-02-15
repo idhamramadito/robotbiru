42290c55a75332f4a2e61ba8f007be53df990ff3
idhamramadito <idhamadito@gmail.com>
2021-02-15 12:05:57 +0700

    page_topup

--- homepage/lib/pages/dashboard_tabs/home/submenu/topup/page_topup.dart
+++ homepage/lib/pages/dashboard_tabs/home/submenu/topup/page_topup.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cupertino_radio_choice/cupertino_radio_choice.dart';
+import 'package:flutter/cupertino.dart';
 
 class TopUp extends StatefulWidget {
   @override
@@ -14,21 +16,152 @@ class _TopUpState extends State<TopUp> {
         title: Text('Top Up Saldo'),
       ),
       body: Container(
-        margin: EdgeInsets.all(20),
+        // margin: EdgeInsets.all(20),
         child: Column(
           children: [
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Jumlah Top Up',
-                textAlign: TextAlign.start,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Jumlah Top Up',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+              decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.grey[100]),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: Text('Rp',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                        )),
+                  ),
+                  Text('50.000',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      )),
+                ],
               ),
             ),
             SizedBox(
-              height: 20,
+              height: 5,
             ),
-            Container()
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Jumlah top up minimal Rp 50.000.',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: CupertinoRadioChoice(choices: {
+                'Rp50.000': 'Rp50.000',
+                'Rp.100.000': 'Rp100.000',
+                'Rp.150.000': 'Rp150.000'
+              }, onChange: (selectedPrice) {}, initialKeyValue: 'Rp50.000'),
+              // child: Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //   children: [
+              //     FlatButton(
+              //       child: Text(
+              //         'Rp50.000',
+              //         style: TextStyle(fontSize: 18),
+              //       ),
+              //       color: Colors.blue,
+              //       textColor: Colors.white,
+              //       padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              //       shape: RoundedRectangleBorder(
+              //         borderRadius: BorderRadius.circular(10.0),
+              //       ),
+              //       onPressed: () {},
+              //     ),
+              //     FlatButton(
+              //       child: Text(
+              //         'Rp100.000',
+              //         style: TextStyle(fontSize: 18),
+              //       ),
+              //       color: Colors.blue,
+              //       textColor: Colors.white,
+              //       padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              //       shape: RoundedRectangleBorder(
+              //         borderRadius: BorderRadius.circular(10.0),
+              //       ),
+              //       onPressed: () {},
+              //     ),
+              //     FlatButton(
+              //       child: Text(
+              //         'Rp150.000',
+              //         style: TextStyle(fontSize: 18),
+              //       ),
+              //       color: Colors.blue,
+              //       textColor: Colors.white,
+              //       padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              //       shape: RoundedRectangleBorder(
+              //         borderRadius: BorderRadius.circular(10.0),
+              //       ),
+              //       onPressed: () {},
+              //     ),
+              //   ],
+              // ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Divider(
+              thickness: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Metode Pembayaran',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              ),
+            ),
+            ListTile(
+              onTap: () {},
+              title: Text(
+                'Bayar Dengan',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              subtitle: Text(
+                'Pilih Metode Pembayaran',
+                style: TextStyle(color: Colors.black),
+              ),
+              leading: Image.asset(
+                'images/kredit.png',
+                height: 40,
+              ),
+              trailing: Icon(
+                Icons.keyboard_arrow_right,
+                size: 40,
+                color: Colors.grey[400],
+              ),
+            )
           ],
         ),
       ),
