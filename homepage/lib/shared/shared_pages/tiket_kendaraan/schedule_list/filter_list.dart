2fcb2c87371c167d6831182faa591e534e879e7c
Qisas Hasanudin <qisas.tazkia@ui.ac.id>
2021-02-15 10:36:03 +0700

    tambah visibility

--- homepage/lib/shared/shared_pages/tiket_kendaraan/schedule_list/filter_list.dart
+++ homepage/lib/shared/shared_pages/tiket_kendaraan/schedule_list/filter_list.dart
@@ -342,9 +342,15 @@ Future filterList(BuildContext context, TransportationModel previousData) {
                     ),
                   ),
                 ),
-                floatingActionButton: FloatingActionButton.extended(
-                  label: Text('Terapkan'),
-                  onPressed: () {},
+                floatingActionButton: Visibility(
+                  // TODO: Kasih visible:
+                  child: FloatingActionButton.extended(
+                    label: Text(
+                      'TERAPKAN',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {},
+                  ),
                 ),
                 floatingActionButtonLocation:
                     FloatingActionButtonLocation.centerFloat,
