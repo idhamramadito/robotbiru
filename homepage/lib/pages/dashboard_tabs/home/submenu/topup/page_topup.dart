f5023785c2eac3fb1ef7a4ca487791a6c24ab412
idhamramadito <idhamadito@gmail.com>
2021-02-15 10:54:18 +0700

    bikin folder topup

---
+++ homepage/lib/pages/dashboard_tabs/home/submenu/topup/page_topup.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TopUp extends StatefulWidget {
+  @override
+  _TopUpState createState() => _TopUpState();
+}
+
+class _TopUpState extends State<TopUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Top Up Saldo'),
+      ),
+      body: ,
+    );
+  }
+}
